82b64e3bb643e4b589ccfe0208174953e7fd0dae
murilolealufca <murilo.leal@aluno.ufca.edu.br>
2018-10-03 03:39:52 -0300

    Primeiro código de conversão de tabelas para o Latex

--- tabelas/tabelas.R
+++ tabelas/tabelas.R
@@ -1,1 +1,17 @@
-#
+library(readxl) #Inicia biblioteca para conversão de tabelas para o Latex
+oleo <- read_excel("Documentos/PRODER/Artigos/oleo/oleo.xlsx") #Abre conjunto de dados
+
+umidademedia<-mean(x = oleo$Umidade) #Calcula e armazena a média
+umidadesd<-sd(x = oleo$Umidade) #Calcula e armazena o desvio padrão
+calciomedia<-mean(x = oleo$Cálcio)
+calciosd<-sd(x = oleo$Cálcio)
+
+tabela<-c(umidademedia,umidadesd,calciomedia,calciosd) #Agrega valores calculados
+tabela<-matrix(data = tabela, nrow = 2, ncol = 2) #Cria tabela com valores agregados
+
+colnames(tabela) <- c("Media", "SD") #Nomeia as colunas
+rownames(tabela) <- c("Humidity", "Calcium") #Nomeia as linhas
+tabela #Mostra tabela montada
+
+xtable(tabela, caption=NULL, label="teste", align=NULL, vsep=NULL, digits=NULL,
+       display=NULL) #Realiza conversão para o Latex
